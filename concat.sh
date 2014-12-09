866b5d991af24ba8f1265f1759da06c2e1a4f4f8
Miyayx <yangxue00.yxmn@gmail.com>
2014-12-09 21:29:18 +0800

    concat success

--- concat.sh
+++ concat.sh
@@ -8,13 +8,15 @@ echo ${title[@]}
 
 for t in ${title[@]}
   do
-    echo $t
     mp3s=($(ls "$t("*.mp3))
-    e=""
+    t="$t".mp3
+    echo $t
+    echo "" > tmp
     for m in ${mp3s[@]}
       do
-         e="$e|${m}"
+         #ffmpeg -i "concat:$t|$m" -i $m -y -acodec copy $t
+         echo file $m >> tmp
       done
-    e=${e:1}
-    ffmpeg -i "concat:$e" -acodec copy "$t".mp3
+    
+     ffmpeg -f concat -i tmp -y -acodec copy $t
   done
