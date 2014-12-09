36bca05488b79cd8cf8b13476c206ce471206bc0
Miyayx <yangxue00.yxmn@gmail.com>
2014-12-09 20:57:49 +0800

    concat modify

--- concat.sh
+++ concat.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+cd new
+
 title=()
 title=(`find . -name '*\).mp3' | cut -d'(' -f 1-2 | cut -d')' -f 1-2`)
+echo ${title[@]}
 
 for t in ${title[@]}
   do
     echo $t
-    ffmpeg -i "$t"*.mp3 -c copy "$t".mp3
+    mp3s=($(ls "$t("*.mp3))
+    e=""
+    for m in ${mp3s[@]}
+      do
+         e="$e|${m}"
+      done
+    e=${e:1}
+    ffmpeg -i "concat:$e" -acodec copy "$t".mp3
   done
--- crawler.sh
+++ crawler.sh
@@ -47,7 +47,7 @@ OUTPUT=new/
 mkdir $OUTPUT
 
 pages=()
-for c in `curl $URL$CLASS/'page'[1-13]/`
+for c in `curl $URL$CLASS/'page'[14-20]/`
 do 
     if echo $c | grep -E "/$CLASS/[0-9]{12}" > /dev/null
     then 
@@ -61,28 +61,23 @@ done
 for p in ${pages[@]}
 do
     echo $URL$p
-    #title=`curl -s $URL$p | sed -e 's///g'|sed -n "/<h3 id=\"listenTitle\">/,/<\/h3>/p" | grep -oP "新编日语.*"`
-    #title=`curl -s $URL$p | sed -e 's///g' | tr '' ' ' | tr '\n' ' ' | tr '\r' ' '|sed -n "/<h3 id=\"listenTitle\">/,/<\/h3>/p" | grep -oP "<h3 id=\"listenTitle\">.*<\/h3>" `
     title=`curl -s $URL$p | sed -e 's///g' | sed -n "/<h3 id=\"listenTitle\">/,/<\/h3>/p" | tr '' ' ' | tr '\n' ' ' | tr '\r' ' '| grep -oP "<\/a>.*<\/h3>" `
     title=${title:4}
     title=${title%%</\h3>}
-    #title=`echo $title | tr -d ' ' | tr '(' '[' | tr ')' ']'`
     title=`echo $title | sed -e 's/^ *//' -e 's/ *$//'`
 
-    #title=`curl -s $URL$p | sed -e 's///g' | grep -E '<h3 id=\"listenTitle\".*</h3>'`
-    #title=`curl -s $URL$p | grep -oP '>\"新编日语.*\"'`
-    #title=`curl -s $URL$p | grep -oP '(?<=<h3 id="listenTitle"> ).*?(?= </h3>)'`
     echo $title
 
     echo "Downloading "$title".mp3 ..."
-    mp3=`curl -s $URL$p | grep -oP "http:\/\/t1.g.hjfile.cn\/listen\/[0-9]{6}\/[0-9]{21}.mp3"`
+    #mp3=$URL`curl -s $URL$p | grep -oP "download\/[0-9]{6}\/"`
+    mp3=`curl -s $URL$p | grep -oP "http:\/\/t1.g.hjfile.cn\/listen\/[0-9]{6}\/[0-9]{20,22}.mp3"`
     #mp3=$mp3"?attach="`urlencode 沪江听写酷_$title`
     #mp3=${mp3%%\+}
     #mp3="http://t1.g.hjfile.cn/listen/201307/201307011014384405433.mp3?attach=%e6%b2%aa%e6%b1%9f%e5%90%ac%e5%86%99%e9%85%b7_%e6%96%b0%e7%bc%96%e6%97%a5%e8%af%ad%e7%ac%ac%e4%b8%80%e5%86%8c(%e4%bf%ae%e8%ae%a2%e7%89%88)+%e7%ac%ac09%e8%af%be+%e5%89%8d%e6%96%87"
     echo URL: $mp3
-    title=`echo $title | tr -d ' ' | tr '/' '\' `
+    title=沪江听写酷_`echo $title | tr ' ' '_' | tr '/' '-' `
     curl -L -l $mp3 > $OUTPUT"$title".mp3 
     echo "$OUTPUT""$title".mp3 Done
-    sleep 30
+    sleep 10
 done
 
